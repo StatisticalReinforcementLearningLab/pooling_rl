c88752dd58ba7e1b18eecb4ab64404d8bd1544ba
Sabina Tomkins <sabina@umiacs.umd.edu>
2018-12-03 10:04:25 -0500

    initialization

--- models/bandit.py
+++ models/bandit.py
@@ -1,2 +1,22 @@
 def feat2_function(z,x):
     return [1,z[0],x]
+
+def function_zero(x):
+    return 0
+
+
+def feat0_function(z,x):
+    temp =  [1]
+    temp.extend(z)
+    temp.append(x)
+    return temp
+
+def feat1_function(z,x):
+    temp =  [1]
+    temp.extend(z)
+    temp.append(x)
+    return temp
+
+def init_mu(val,length):
+    return [val]* length
+
