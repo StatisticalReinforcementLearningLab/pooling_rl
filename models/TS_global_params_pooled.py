695c8048811565e81d3138db159e2f42cb47c2ac
Sabina321 <sabina.tomkins@gmail.com>
2019-02-20 21:38:05 -0500

    try one hot et ceterag

--- models/TS_global_params_pooled.py
+++ models/TS_global_params_pooled.py
@@ -205,7 +205,7 @@ class TS_global_params:
         self.updated_cov=True
 
     def get_theta(self,dim_baseline):
-        m = 100*np.eye(dim_baseline)
+        m = 1000*np.eye(dim_baseline)
         #m = np.add(m,.1)
         return m
 
