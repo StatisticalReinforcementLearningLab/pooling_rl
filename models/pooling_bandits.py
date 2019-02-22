1c6f0bb6c05bc9ab831f01aa23b002229f3261e4
Sabina321 <sabina.tomkins@gmail.com>
2019-02-22 00:54:28 -0500

    g

--- models/pooling_bandits.py
+++ models/pooling_bandits.py
@@ -81,8 +81,8 @@ def run(X,y,global_params,gp_train_type='Static'):
     
     with tf.Graph().as_default():
         
-        gp.reset_default_session(graph=graph)
-        sess = gp.get_default_session()
+        gpflow.reset_default_session(graph=graph)
+        sess = gpflow.get_default_session()
         print(global_params.kdim)
         
         if gp_train_type=='empirical_bayes':
