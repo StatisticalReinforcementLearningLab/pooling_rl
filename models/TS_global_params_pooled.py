83805fb240d392c34035807f0e496dfb9436803a
Sabina321 <sabina.tomkins@gmail.com>
2019-02-21 23:50:02 -0500

    test speed

--- models/TS_global_params_pooled.py
+++ models/TS_global_params_pooled.py
@@ -45,7 +45,8 @@ class TS_global_params:
         self.sigma_theta =self.get_theta(self.theta_dim)
         #self.sigma_v=.5*np.eye(2)
         
-        self.sigma_v =
+        self.sigma_v =np.array([[4492.02905157 ,   0.0        ]
+                                [   0.0 ,        2027.39758508]])
             #np.array([[84268.04299068,     0.0        ],[    0.0,         13464.64044625]])
         #self.sigma_v = np.array([[5.21906177e-00, 0.00000000e+00],
         # [0.00000000e+00, 1.00000000e-2]])
