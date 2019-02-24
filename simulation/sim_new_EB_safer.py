d333a6cb7f6ff0e80cdf5e98557f2d5de049db67
Sabina321 <sabina.tomkins@gmail.com>
2019-02-24 07:04:26 -0500

    t

--- simulation/sim_new_EB_safer.py
+++ simulation/sim_new_EB_safer.py
@@ -84,7 +84,7 @@ def new_kind_of_simulation(experiment,policy=None,personal_policy_params=None,gl
         #history  = pb.make_history(experiment)
         if time==experiment.last_update_day+pd.DateOffset(days=global_policy_params.update_period):
             experiment.last_update_day=time
-            print('Global update', time,global_policy_params.decision_times,time_module.strftime('%l:%M%p %Z on %b %d, %Y'),file=open('updates_{}_{}.txt'.format(len(experiment.population),global_policy_params.update_period), 'a'))
+            print('Global update', time,global_policy_params.decision_times,time_module.strftime('%l:%M%p %Z on %b %d, %Y'),file=open('updates_safer_{}_{}.txt'.format(len(experiment.population),global_policy_params.update_period), 'a'))
             if global_policy_params.decision_times>200:
                 glob.last_global_update_time=time
                 history =pb.make_history_new(.1,global_policy_params,experiment)
@@ -249,7 +249,7 @@ def new_kind_of_simulation(experiment,policy=None,personal_policy_params=None,gl
                     to_save = make_to_save(experiment)
                     gids = make_to_groupids(experiment)
                     
-                    filename = '{}/results/population_size_{}_update_days_{}_{}_EB_{}_{}_testing_{}.pkl'.format('../../murphy_lab/lab/pooling',pop_size,update_time,study_length,case,sim_num,global_policy_params.decision_times)
+                    filename = '{}/results/population_size_{}_update_days_{}_{}_EB_{}_{}_testing_{}_safer.pkl'.format('../../murphy_lab/lab/pooling',pop_size,update_time,study_length,case,sim_num,global_policy_params.decision_times)
                     with open(filename,'wb') as f:
                         pickle.dump({'history':to_save,'gids':gids,'likelis':glob.to_save_params},f)
 
@@ -287,7 +287,7 @@ def run_many(start_index,end_index):
             to_save = make_to_save(experiment)
             gids = make_to_groupids(exp)
     
-            filename = '{}/results/population_size_{}_update_days_{}_{}_EB_{}_{}_testing.pkl'.format('../../murphy_lab/lab/pooling',population,update_time,study_length,case,sim_num)
+            filename = '{}/results/population_size_{}_update_days_{}_{}_EB_{}_{}_testing_safer.pkl'.format('../../murphy_lab/lab/pooling',population,update_time,study_length,case,sim_num)
             with open(filename,'wb') as f:
                 pickle.dump({'history':to_save,'gids':gids,'likelis':glob.to_save_params},f)
           
@@ -319,6 +319,6 @@ if __name__=="__main__":
             to_save = make_to_save(experiment)
             gids = make_to_groupids(experiment)
             
-            filename = '{}/results/population_size_{}_update_days_{}_{}_EB_{}_{}_testing_final.pkl'.format('../../murphy_lab/lab/pooling',pop_size,update_time,study_length,case,i)
+            filename = '{}/results/population_size_{}_update_days_{}_{}_EB_{}_{}_testing_final_safer.pkl'.format('../../murphy_lab/lab/pooling',pop_size,update_time,study_length,case,i)
             with open(filename,'wb') as f:
                 pickle.dump({'history':to_save,'gids':gids,'likelis':glob.to_save_params},f)
