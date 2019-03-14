a3922578e17105fee687f11006ddf6ad3aeb99f9
Sabina321 <sabina.tomkins@gmail.com>
2019-03-14 14:01:36 -0400

    run on server

--- simulation/sim_batch_baseline.py
+++ simulation/sim_batch_baseline.py
@@ -12,7 +12,7 @@ import sim_functions_cleaner  as sf
 import operator
 import study
 import time as time_module
-import run_gpy
+import run_gpy_simple
 import TS_personal_params_pooled as pp
 import TS_global_params_pooled as gtp
 from numpy.random import uniform
