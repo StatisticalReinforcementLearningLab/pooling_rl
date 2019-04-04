5ab871de6a37f9687e0d03813d8072e43b253496
Sabina321 <sabina.tomkins@gmail.com>
2019-04-03 22:30:41 -0400

    small sigma u

--- simulation/sim_new_EB_ft.py
+++ simulation/sim_new_EB_ft.py
@@ -108,6 +108,7 @@ def new_kind_of_simulation(experiment,policy=None,personal_policy_params=None,gl
                 print(history[2].mean())
                 print(y_adjusted.mean())
                 print(y_adjusted.std())
+                print(experiment.beta)
                 print(personal_policy_params.mus2[0])
                 print(len(history))
                 #print(y_adjusted)
