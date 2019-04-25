723b783dad42ef5ec89b9f47a652c5c94e1e8d4d
Sabina321 <sabina.tomkins@gmail.com>
2019-04-25 10:18:10 -0400

    bet starts

--- models/run_gpytorchkernel.py
+++ models/run_gpytorchkernel.py
@@ -254,7 +254,7 @@ def run(X,users,y,global_params):
                     f_covar = f_preds.covariance_matrix
                     covtemp = f_covar.detach().numpy()
                     print('noise two {}'.format(likelihood.second_noise_covar.noise.item()))
-                    print('noise two {}'.format(likelihood.noise_covar.noise.item()))
+                    print('noise two {}'.format(likelihood.noise_covar))
                     if np.isreal(sigma_temp).all() and not np.isnan(covtemp).all() and eigs[0][0]>0.005 and eigs[0][1]>0.005:
                         sigma_u = sigma_temp
                         cov=covtemp
