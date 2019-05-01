ac185f85002af0b158a74836ec085f162f28056b
Sabina321 <sabina.tomkins@gmail.com>
2019-05-01 13:37:32 -0400

    training

--- simulation/runner_server.py
+++ simulation/runner_server.py
@@ -20,8 +20,10 @@ if __name__=="__main__":
     start_index = sys.argv[4]
     end_index = sys.argv[5]
     case =sys.argv[6]
+    train_type =sys.argv[7]
+    
     root = 'pooling/distributions/'
     write_directory = 'pooling/results/'
-    sim_all.run_many('pooling',[case],int(start_index),int(end_index),int(update_time),root,write_directory)
+    sim_all.run_many('pooling',[case],int(start_index),int(end_index),int(update_time),root,write_directory,train_type)
 
 
