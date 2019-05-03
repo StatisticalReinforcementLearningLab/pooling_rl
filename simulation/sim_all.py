43f01368b1243f6139b47179c68ca17b0ab5d50c
Sabina321 <sabina.tomkins@gmail.com>
2019-05-03 13:43:36 -0400

    try noise tuning

--- simulation/sim_all.py
+++ simulation/sim_all.py
@@ -173,7 +173,7 @@ def new_kind_of_simulation(experiment,policy=None,personal_policy_params=None,gl
                     
                     #####HERE GET NEW NOISE TERM
                     if train_type=='EB':
-                        temp_params = run_gpytorchkernel.run(temp_data[0], temp_data[1],steps,global_policy_params
+                        temp_params = run_gpytorchkernel.run(temp_data[0], temp_data[1],steps,global_policy_params)
                         global_policy_params.noise_term = temp_params['noise']
                     temp = TS.policy_update_ts_new( context,steps,probs,actions,global_policy_params.noise_term,\
                                                        global_policy_params.mu1_knot,\
