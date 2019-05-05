9c88a1d4140cc36656cbfe30a7ffbfc8a5ec92c3
Sabina321 <sabina.tomkins@gmail.com>
2019-05-05 13:34:38 -0400

    try noise tuning

--- models/run_gpytorchkernel.py
+++ models/run_gpytorchkernel.py
@@ -74,8 +74,8 @@ class MyKernel(Kernel):
         
         
         #initial_value=init_u2*torch.tensor(1.0)
-        self.register_constraint("raw_u1",constraint= constraints.Positive())
-        self.register_constraint("raw_u2",constraint= constraints.Positive())
+        self.register_constraint("raw_u1",constraint= constraints.Positive(initial_value=init_u1*torch.tensor(1.0)))
+        self.register_constraint("raw_u2",constraint= constraints.Positive(initial_value=init_u2*torch.tensor(1.0)))
         
         self.register_constraint("raw_rho",constraint= constraints.Interval(0.0,2.0))
         print(self.rho)
