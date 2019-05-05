972a1f6029bfa911ba209b5da1dbead08e74a578
Sabina321 <sabina.tomkins@gmail.com>
2019-05-05 09:33:11 -0400

    try noise tuning

--- models/run_gpytorchkernel.py
+++ models/run_gpytorchkernel.py
@@ -277,7 +277,7 @@ def run(X,users,y,global_params):
                                   
         mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
         #def train(num_iter):
-        num_iter=25
+        num_iter=5
         with gpytorch.settings.use_toeplitz(False):
             for i in range(num_iter):
                 try:
