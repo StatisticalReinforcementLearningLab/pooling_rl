4c26743f5fcda11572916d77397e07542e012c1c
Sabina321 <sabina.tomkins@gmail.com>
2019-05-27 13:10:33 +0200

    run this one

--- simulation/sim_all.py
+++ simulation/sim_all.py
@@ -443,7 +443,7 @@ def run_many(algo_type,cases,sim_start,sim_end,update_time,dist_root,write_direc
         #'dow','pretreatment',
         #'tod','dow',
         baseline = ['tod','dow','pretreatment','location']
-        responsivity_keys = ['tod','dow','pretreatment','location']
+        responsivity_keys = ['tod','pretreatment','location']
         
         
         for u in [update_time]:
@@ -471,7 +471,7 @@ def run_many(algo_type,cases,sim_start,sim_end,update_time,dist_root,write_direc
                 
             
             
-                filename = '{}{}/population_size_{}_update_days_{}_{}_static_sim_{}_523_tdpl4r.pkl'.format('{}{}/'.format(write_directory,algo_type),case,pop_size,u,'short',sim)
+                filename = '{}{}/population_size_{}_update_days_{}_{}_static_sim_{}_523_tpl4r.pkl'.format('{}{}/'.format(write_directory,algo_type),case,pop_size,u,'short',sim)
                 with open(filename,'wb') as f:
                     pickle.dump({'gids':gids,'regrets':rewards,'actions':actions,'history':to_save,'pprams':personal,'gparams':glob},f)
         #filename = '{}/results/{}/population_size_{}_update_days_{}_{}_static_sim_regrets_actions_l_prelocb.pkl'.format('../../Downloads/pooling_results/{}/'.format(algo_type),case,pop_size,u,'short')
